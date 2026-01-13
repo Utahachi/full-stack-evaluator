7d796653ca06783ebccc2379bfeabb99e97957d8
Utahachi <philipvergara0524@gmail.com>
2026-01-13 15:00:11 +0800

    chore: fixing CORS issue

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -6,7 +6,7 @@ function TaskList() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:5215/tasks') // <-- replace with your backend URL
+    axios.get('http://localhost:5215/tasks')
       .then(res => setTasks(res.data))
       .catch(err => {
         console.error('Error fetching tasks:', err);
